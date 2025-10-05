f186b54b85c39f32bf529e1b7d212c9d2d15298c
Paradon Thonthong(Burger) <paradon.thth@gmail.com>
2025-10-05 07:21:45 +0700

    edit footer [email]

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC<FooterProps> = ({isDarkMode}) => {
     return (
         <div className="mt-20">
             <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-                <div className="flex items-center gap-2">
+                <div className="flex items-center justify-center gap-2 mb-4 sm:mb-0">
                     <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt="" className="w-6" />
                     paradon.thth@gmail.com
                 </div>
